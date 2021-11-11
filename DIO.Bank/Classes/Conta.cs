aaefd2ce9ac0127e8e27ba9a148deac026093960
Paulo Roberto de Alencar <paulo.alencar@unifai.local>
2021-11-11 11:08:42 -0300

    Criação da classe Conta e enumerador TipoConta

---
+++ DIO.Bank/Classes/Conta.cs
@@ -0,0 +1,19 @@
+namespace DIO.Bank
+{
+	public class Conta
+	{
+		public TipoConta TipoConta { get; set; }
+		public double Saldo { get; set; }
+		public double Credito { get; set; }
+		public string Nome { get; set; }
+
+		public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+		{
+			this.TipoConta = tipoConta;
+			this.Saldo = saldo;
+			this.Credito = credito;
+			this.Nome = nome;
+		}
+
+	}
+}
